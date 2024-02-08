c9a909894097fa4457bc98b2e8c73d97f5b8b740
Diam <trejodev@gmail.com>
2024-02-08 11:12:39 +0100

    First commit

---
+++ src/app.http.ts
@@ -0,0 +1,34 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+  //   const data = { name: "John Doe", age: 30, city: "New York" };
+  //   res.writeHead(200, { "Content-type": "application/json" });
+  //   res.end(JSON.stringify(data));
+
+  if (req.url === "/") {
+    const htmlFile = fs.readFileSync("./public/index.html", "utf-8");
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(htmlFile);
+    return;
+  }
+
+//! Express, Fastify => Nest.js
+  if (req.url?.endsWith(".js")) {
+    const jsFile = fs.readFileSync("./public/js/app.js");
+    res.writeHead(200, { "Content-type": "application/javascript" });
+    res.end(jsFile);
+    return;
+  } else if (req.url?.endsWith(".css")) {
+    const cssFile = fs.readFileSync("./public/css/styles.css");
+    res.writeHead(200, { "Content-type": "text/css" });
+    res.end(cssFile);
+  }
+
+  const responseContent = fs.readFileSync(`./public${req.url}`, "utf-8");
+  res.end(responseContent);
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
