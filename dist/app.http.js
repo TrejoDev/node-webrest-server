99885858f595637cf74c8d1aa520cd4e7bdec22e
Diam <trejodev@gmail.com>
2024-02-15 04:36:54 +0100

    Add Postgres

---
+++ dist/app.http.js
@@ -0,0 +1,36 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const server = http_1.default.createServer((req, res) => {
+    //   const data = { name: "John Doe", age: 30, city: "New York" };
+    //   res.writeHead(200, { "Content-type": "application/json" });
+    //   res.end(JSON.stringify(data));
+    var _a, _b;
+    if (req.url === "/") {
+        const htmlFile = fs_1.default.readFileSync("./public/index.html", "utf-8");
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end(htmlFile);
+        return;
+    }
+    //! Express, Fastify => Nest.js
+    if ((_a = req.url) === null || _a === void 0 ? void 0 : _a.endsWith(".js")) {
+        const jsFile = fs_1.default.readFileSync("./public/js/app.js");
+        res.writeHead(200, { "Content-type": "application/javascript" });
+        res.end(jsFile);
+        return;
+    }
+    else if ((_b = req.url) === null || _b === void 0 ? void 0 : _b.endsWith(".css")) {
+        const cssFile = fs_1.default.readFileSync("./public/css/styles.css");
+        res.writeHead(200, { "Content-type": "text/css" });
+        res.end(cssFile);
+    }
+    const responseContent = fs_1.default.readFileSync(`./public${req.url}`, "utf-8");
+    res.end(responseContent);
+});
+server.listen(8080, () => {
+    console.log("Server running on port 8080");
+});
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,81 +1,92 @@
 import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
-const todos = [
-  { id: 1, text: "buy Milk", completedAt: new Date() },
-  { id: 2, text: "buy bread", completedAt: null },
-  { id: 3, text: "buy butter", completedAt: new Date() },
-];
+// const todos = [
+//   { id: 1, text: "buy Milk", completedAt: new Date() },
+//   { id: 2, text: "buy bread", completedAt: null },
+//   { id: 3, text: "buy butter", completedAt: new Date() },
+// ];
 
 export class TodosController {
   // *DI
   constructor() {}
 
-  public getTodos = (req: Request, res: Response) => {
+  public getTodos = async (req: Request, res: Response) => {
+    const todos = await prisma.todo.findMany({});
+
     return res.json(todos);
   };
 
-  public getTodoById = (req: Request, res: Response) => {
+  public getTodoById = async (req: Request, res: Response) => {
     const id = +req.params.id; //*  Signo "+" convierte un string => number
     if (isNaN(id))
       return res.status(400).json({ error: "ID argument is not a number" });
 
-    const todo = todos.find((todo) => todo.id === id);
+    const todo = await prisma.todo.findUnique({
+      where: { id },
+    });
 
     todo
       ? res.json(todo)
       : res.status(404).json({ error: `TODO with id ${id} not found` });
   };
 
-  public createTodo = (req: Request, res: Response) => {
-    const { text } = req.body;
-    if (!text) res.status(400).json({ error: "Text property is required" });
+  public createTodo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
 
-    const newTodo = {
-      id: todos.length + 1,
-      text: text,
-      completedAt: null,
-    };
+    if (error) return res.status(400).json({ error });
 
-    todos.push(newTodo);
-
-    res.json(newTodo);
-  };
-
-  public updateTodo = (req: Request, res: Response) => {
-    const id = +req.params.id;
-    if (isNaN(id))
-      return res.status(400).json({ error: "ID argument is not a number" });
-
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo)
-      return res.status(404).json({ error: `Todo with id ${id} not found` });
-
-    const { text, completedAt = new Date() } = req.body;
-    // if (!text) res.status(400).json({ error: "Text property is required" });
-
-    todo.text = text || todo.text; //! Ojo referencia. Los obj en Js psan x referencia. Es una mala practica xq no se debe de mutar la informacion.
-    completedAt === "null"
-      ? (todo.completedAt = null)
-      : (todo.completedAt = new Date(completedAt || todo.completedAt));
+    const todo = await prisma.todo.create({
+      data: createTodoDto!,
+    });
 
     res.json(todo);
   };
 
-  public deleteTodo = (req: Request, res: Response) => {
+  public updateTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    const [error, updateTodoDto] = UpdateTodoDto.update({
+      ...req.body,
+      id,
+    });
+
+    if (error) return res.status(400).json({ error });
+
+    try {
+      const todo = await prisma.todo.update({
+        where: { id },
+        data: updateTodoDto!.values,
+      });
+
+      res.json(todo);
+    } catch (error) {
+      return res.status(404).json({ error: `Todo with id ${id} not found` });
+    }
+
+    // todo.text = text || todo.text; //! Ojo referencia. Los obj en Js psan x referencia. Es una mala practica xq no se debe de mutar la informacion.
+  };
+
+  public deleteTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
     if (isNaN(id))
       return res.status(400).json({ error: "ID argument is not a number" });
 
-    const todo = todos.find((todo) => todo.id === id);
-    if (!todo)
-      return res.status(404).json({ error: `Todo with id ${id} not found` });
+    try {
+      const deleted = await prisma.todo.delete({
+        where: { id },
+      });
 
-    todos.splice(todos.indexOf(todo), 1);
-    console.log(todos)
-    res.json({
-      ok: true,
-      message: `Todo with id ${id} was deleted`,
-      todo,
-    });
+      res.json({
+        ok: true,
+        message: `Todo with id ${id} was deleted`,
+        deleted,
+      });
+    } catch (error) {
+      return res.status(404).json({ error: `Todo with id ${id} not found` });
+    }
+
+    // todos.splice(todos.indexOf(todo), 1);
   };
 }
